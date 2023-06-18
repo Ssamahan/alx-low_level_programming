c349c0a8148089f7720a195d6c9bb20093878153
Ssamahan <smhnklvn@gmail.com>
2023-06-18 13:34:51 -0700

    a C program that prints exactly with proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a C program that prints exactly with proper grammar
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
