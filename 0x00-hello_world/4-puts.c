1b03a8039ee58c1ebdd3b4abee6c514b42cc93da
Ssamahan <smhnklvn@gmail.com>
2023-06-18 13:17:41 -0700

    C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print exactly a sentence
+ *
+ * Return: value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
