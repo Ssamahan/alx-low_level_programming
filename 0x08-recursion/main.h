d2ccfd4203f0abc3c987b60ed42c78058eee193b
Ssamahan <smhnklvn@gmail.com>
2023-07-08 03:01:03 -0700

    compares two strings

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
+
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
@@ -10,4 +12,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
