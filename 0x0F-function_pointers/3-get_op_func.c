9db914a8fe2ded1fab711fd913aa46c2e13f046d
Ssamahan <smhnklvn@gmail.com>
2023-07-19 14:35:35 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - Selects the correct function to perform the operation asked
+ * get_op_func - Selects the correct function 
+ * 			to perform the operation asked
  * @s: The operator passed as argument
- * Return: A pointer to the function corresponding to the operator given as a parameter.
+ * Return: A pointer to the function corresponding 
+ * 			to the operator given as a parameter.
  */
 
 int (*get_op_func(char *s))(int, int)
